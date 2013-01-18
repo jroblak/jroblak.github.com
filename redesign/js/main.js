e7b3e850d2e24c5a095cafe3fbd938cd760e5d6d
Justin Oblak <jroblak@gmail.com>
2013-01-18 17:04:26 -0500

    testing layout idea

---
+++ redesign/js/main.js
@@ -0,0 +1,16 @@
+;$(function(){
+	
+	$('.innercontain').click(function() {
+		var par = $(event.target).parent();
+		if (par.hasClass('skills')) {
+			
+		} else if (par.hasClass('about')) {
+			
+		} else if (par.hasClass('portfolio')) {
+			
+		} else {
+			
+		} 
+	});
+	
+});
