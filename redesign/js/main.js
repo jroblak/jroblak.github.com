ae8cabb927f4a1ff8ac26a4f57152975a837ae61
Justin Oblak <jroblak@gmail.com>
2013-01-18 17:06:18 -0500

    comment

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -1,5 +1,8 @@
 ;$(function(){
 	
+	// First, set up size of the content div based on the screen size
+	
+	// Next, set up the click handlers for displaying that content
 	$('.innercontain').click(function() {
 		var par = $(event.target).parent();
 		if (par.hasClass('skills')) {
@@ -8,7 +11,7 @@
 			
 		} else if (par.hasClass('portfolio')) {
 			
-		} else {
+		} else	 {
 			
 		} 
 	});
