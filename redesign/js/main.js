bc30e75cbd7037ef1207257dcd8b2ed2c223802f
Justin Oblak <jroblak@gmail.com>
2013-01-19 11:30:15 -0500

    more css

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -3,19 +3,24 @@
 	// First, set up size of the content div based on the screen size
 	
 	// Next, set up the click handlers for displaying that content
-	$('.innercontain').click(function() {
-		var par = $(event.target).parent();
-		$('.content').load('portfolio/index.html').hide().fadeIn();
-		
-		if (par.hasClass('skills')) {
+	$('.nav').click(function() {
+
+		//$('.content').load('portfolio/index.html').hide().fadeIn();
+		if ($(this).is('#skills')) {
+			console.log('skills');
+		} else if ($(this).is('#about')) {
 			
-		} else if (par.hasClass('about')) {
-			
-		} else if (par.hasClass('portfolio')) {
+		} else if ($(this).is('#portfolio')) {
 			
 		} else	 {
 			
 		} 
+	});
+	
+	$('.nav').hover(function() {
+		$(this).addClass('highlight');
+	}, function() {
+		$(this).removeClass('highlight');
 	});
 	
 });
