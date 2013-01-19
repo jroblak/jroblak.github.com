2e6d939cefb5b01b2dc785b5f400adbcf085c292
Justin Oblak <jroblak@gmail.com>
2013-01-19 12:27:18 -0500

    a big commit - more css, javascript, and content changes

--- redesign/js/main.js
+++ redesign/js/main.js
@@ -1,26 +1,36 @@
 ;$(function(){
 	
 	// First, set up size of the content div based on the screen size
+	var height = $(window).height(); 
+	var width = $(window).width();
+	
+	var contentDiv = width - 700;
+	
+	$('.rightcontain').width(contentDiv);
 	
 	// Next, set up the click handlers for displaying that content
-	$('.nav').click(function() {
-
-		//$('.content').load('portfolio/index.html').hide().fadeIn();
-		if ($(this).is('#skills')) {
-			console.log('skills');
-		} else if ($(this).is('#about')) {
-			
-		} else if ($(this).is('#portfolio')) {
-			
-		} else	 {
-			
-		} 
-	});
+	var loaded = false;
+	var selected = null;
 	
-	$('.nav').hover(function() {
-		$(this).addClass('highlight');
-	}, function() {
-		$(this).removeClass('highlight');
+	$('.nav').click(function() {
+		$(this)
+		if ($(this).is('#skills')) {
+			$(this).addClass('highlight').css('font-size', 24);
+			$(this).siblings().css('font-size', 10).removeClass('highlight');
+			$('.content').load('pages/skills.html').hide().fadeIn();
+		} else if ($(this).is('#about')) {
+			$(this).addClass('highlight').css('font-size', 24);
+			$(this).siblings().css('font-size', 10).removeClass('highlight');
+			$('.content').load('pages/about.html').hide().fadeIn();
+		} else if ($(this).is('#portfolio')) {
+			$(this).addClass('highlight').css('font-size', 24);
+			$(this).siblings().css('font-size', 10).removeClass('highlight');
+			$('.content').load('portfolio/index.html').hide().fadeIn();
+		} else	 {
+			$(this).addClass('highlight').css('font-size', 24);
+			$(this).siblings().css('font-size', 10).removeClass('highlight');
+			$('.content').load('pages/contact.html').hide().fadeIn();
+		} 
 	});
 	
 });
