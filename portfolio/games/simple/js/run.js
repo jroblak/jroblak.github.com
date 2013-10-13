a4c1f77432d264407e525311d21f7db4ded47ecf
Justin Oblak <jroblak@gmail.com>
2013-10-13 19:52:57 -0400

    more cleanup

--- portfolio/games/simple/js/run.js
+++
@@ -1,4 +0,0 @@
-// Just starts the onload function when all the other js files are loaded.
-window.onReady(function() {	
-	game.onload();
-});
--- portfolio/games/spidersoldiers/README.md
+++
@@ -1,4 +0,0 @@
-spider*soldiers*
-===========
-
-An online multiplayer game powered by melonJS, node.js, and socket.io
