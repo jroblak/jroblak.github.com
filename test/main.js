4ecdfc1b4cf48fa9e0a10364e3b93baeeab0b6ea
Justin Oblak <jroblak@gmail.com>
2014-07-24 08:44:50 -0400

    small changes

--- test/main.js
+++ test/main.js
@@ -7,16 +7,16 @@ $(function() {
     window.h = $(window).height();
     window.particles = [];
     window.maxParticles = 100;
-    window.colors = [{r:'255', g:'184', b:'54', a:'0.7'}, 
-                     {r:'92', g:'95', b:'114', a:'0.7'}, 
-                     {r:'125', g:'127', b:'125', a:'0.7'}, 
+    window.colors = [{r:'255', g:'184', b:'54',  a:'0.7'},
+                     {r:'92',  g:'95',  b:'114', a:'0.7'},
+                     {r:'125', g:'127', b:'125', a:'0.7'},
                      {r:'238', g:'239', b:'247', a:'0.7'}];
     window.windStrength = 0;
     window.windDirection = 0;
     window.windTime = 0;
 
     canvas.width = w;
-    canvas.height = h;    
+    canvas.height = h;
 
     ctx.viewportWidth = canvas.width;
     ctx.viewportHeight = canvas.height;
@@ -26,14 +26,14 @@ $(function() {
     loop();
 });
 
-function Particle() {
+function Particle(maxY) {
+    this.direction = Math.floor(Math.random() * 2) == 1 ? 1 : -1;
     this.x = Math.round(Math.random() * w);
     this.vx = 1;
     this.y = -1 * Math.round((Math.random() * h) + 1);
     this.radius = Math.round(Math.random() * 2) + 1;
     this.color = colors[Math.round(Math.random() * 3)];
-    this.changeTimer = 0;
-    this.startDir = Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+    this.changeTimer = Math.round((Math.random() * 50) + 1);
 };
 
 function createParticles(count) {
@@ -52,16 +52,24 @@ function removeParticles(idxs) {
 };
 
 function move(particle) {
-    particle.x += Math.floor(Math.random() * 2) == 1 ? 1 : -1;
+    // todo - some kind of easing function here to slow the x movement over time
+    particle.x += particle.vx * particle.direction;
+    particle.changeTimer -= 1;
     particle.y += 1;
+
+    if (particle.changeTimer <= 0) {
+        particle.vx = 1;
+        particle.direction *= -1;
+        particle.changeTimer = Math.round((Math.random() * 100) + 50);
+    }
 };
 
 function draw() {
     var removals = [];
     ctx.clearRect(0, 0, w, h);
-    ctx.globalCompositeOperation = 'darker'; // overlapping elements get darkened
+    ctx.globalCompositeOperation = 'darker';
 
-    // calculate wind
+    // todo - calculate wind based on mouse movement
 
     for(var i = 0; i < maxParticles; i++){
         var particle = particles[i];
@@ -71,9 +79,8 @@ function draw() {
         if (particle.x > w || particle.y > h) {
             removals.push(i);
         } else {
-            // draw particle
             ctx.fillStyle = 'rgba(' + particle.color.r +
-                                ',' + particle.color.g + 
+                                ',' + particle.color.g +
                                 ',' + particle.color.b +
                                 ',' + particle.color.a + ')';
             ctx.beginPath();
