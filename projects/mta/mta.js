677bd9cc4e741b1e0f29a1ecbd8aa2374d8fb255
Justin Oblak <jroblak@gmail.com>
2014-08-08 16:24:45 -0400

    three major lines drawning done

--- projects/mta/mta.js
+++ projects/mta/mta.js
@@ -36,6 +36,9 @@ var mta = function() {
         ctx.fillStyle = 'white';
         ctx.stroke();
         ctx.fill();
+
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(193, 230, 170, 190);
     }
 
     function run() {
@@ -44,26 +47,146 @@ var mta = function() {
         loop();
     }
 
-    var fourFiveSix = function() {
+    function fourFiveSix() {
+        var color = '#00933C';
+        var delaycolor = 'red';
+        var changecolor = 'yellow';
+
         ctx.beginPath();
         ctx.arc(400, 210, 5, 0, Math.PI * 2, true);
-        ctx.moveTo(398, 210);
-        ctx.lineTo(398, 750);
         ctx.arc(400, 750, 5, 0, Math.PI * 2, true);
-        ctx.lineTo(403, 750);
-        ctx.lineTo(403, 210);
+        ctx.closePath();
+
+        if (currData.line[1].status !== "GOOD SERVICE") {
+            ctx.fillStyle = delaycolor;
+        } else ctx.fillStyle = color;
+        ctx.fill();
+
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(400, 210);
+        ctx.lineTo(400, 750);
+        ctx.closePath();
+
+        if (currData.line[1].status !== "GOOD SERVICE") {
+            ctx.strokeStyle = delaycolor;
+        } else ctx.strokeStyle = color;
+        ctx.stroke();
+    }
+
+    function ace() {
+        var color = '#2850AD';
+        var delaycolor = 'red';
+        var changecolor = 'yellow';
+
+        ctx.beginPath();
+        ctx.arc(150, 5, 5, 0, Math.PI * 2, true);
+        ctx.arc(450, 690, 5, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.arc(435, 435, 5, 0, Math.PI * 2, true);
         ctx.closePath();
         if (currData.line[1].status !== "GOOD SERVICE") {
-            ctx.fillStyle = 'red';
-        } else ctx.fillStyle = '#00933C';
-        ctx.strokeStyle = '#005420';
+            ctx.fillStyle = delaycolor;
+        } else ctx.fillStyle = color;
         ctx.fill();
+
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(150, 5);
+        ctx.lineTo(150, 40);
+        ctx.lineTo(190, 100);
+        ctx.lineTo(190, 580);
+        ctx.lineTo(300, 580);
+        ctx.lineTo(300, 690);
+        ctx.lineTo(450, 690);
+        if (currData.line[1].status !== "GOOD SERVICE") {
+            ctx.strokeStyle = delaycolor;
+        } else ctx.strokeStyle = color;
         ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(190, 435);
+        ctx.lineTo(435, 435);
+        ctx.stroke();
+    }
+
+    function jz() {
+        var color = '#996633';
+        var delaycolor = '';
+        var changecolor = '';
+
+    }
+
+    function oneTwoThree() {
+        var color = '#EE352E';
+        var delaycolor = 'darkred';
+        var changecolor = 'yellow';
+
+        ctx.beginPath();
+        ctx.arc(190, 5, 5, 0, Math.PI * 2, true);
+        ctx.arc(250, 723, 5, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.arc(245, 170, 5, 0, Math.PI * 2, true);
+        ctx.closePath();
+        ctx.arc(285, 165, 5, 0, Math.PI * 2, true);
+        ctx.closePath();
+        if (currData.line[1].status !== "GOOD SERVICE") {
+            ctx.fillStyle = delaycolor;
+        } else ctx.fillStyle = color;
+        ctx.fill();
+
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(190, 5);
+        ctx.lineTo(170, 40);
+        ctx.lineTo(170, 390);
+        ctx.lineTo(250, 490);
+        ctx.lineTo(250, 723);
+        if (currData.line[1].status !== "GOOD SERVICE") {
+            ctx.strokeStyle = delaycolor;
+        } else ctx.strokeStyle = color;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(170, 250);
+        ctx.lineTo(255, 250);
+        ctx.lineTo(255, 170);
+        ctx.lineTo(245, 170);
+        ctx.lineTo(255, 170);
+        ctx.lineTo(255, 180);
+        ctx.lineTo(285, 165);
+        ctx.stroke();
+    }
+
+    function bdfm() {
+        var color = '#FF6319';
+        var delaycolor = '';
+        var changecolor = '';
+    }
+
+    function s() {
+        var color = '#808183';
+        var delaycolor = '';
+        var changecolor = '';
+    }
+
+    function nqr() {
+        var color = '#FCCC0A';
+        var delaycolor = '';
+        var changecolor = '';
+    }
+
+    function seven() {
+        var color = '#B933AD';
+        var delaycolor = '';
+        var changecolor = '';
     }
 
     function draw() {
         if (currData === null) return;
         fourFiveSix();
+        oneTwoThree();
+        ace();
     };
 
     function loop() {
